13d133ff6551b7b3deeb21740ab8a93a2c7f8034
Cristian Se <114921009+NXTCHRIS@users.noreply.github.com>
2024-07-22 14:27:26 +0300

    create initial stage

---
+++ Social-Media-App-Clone/src/App.jsx
@@ -0,0 +1,13 @@
+import reactLogo from "./assets/noun-groot-3369771.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline text-center">Hello world!</h1>
+      <img src={reactLogo} alt="" width={300} />
+    </>
+  );
+}
+
+export default App;
