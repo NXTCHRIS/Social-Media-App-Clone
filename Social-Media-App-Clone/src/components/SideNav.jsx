2570dc55e734f8b085074f432cb4278606a9344a
Cristian Se <114921009+NXTCHRIS@users.noreply.github.com>
2024-07-24 12:16:52 +0300

    add icons to wireframe

--- Social-Media-App-Clone/src/components/SideNav.jsx
+++ Social-Media-App-Clone/src/components/SideNav.jsx
@@ -1,3 +1,11 @@
+import { IoGameControllerSharp } from "react-icons/io5";
+import { BsArrowUpRightSquareFill } from "react-icons/bs";
+import { MdMovie } from "react-icons/md";
+import { MdSportsBaseball } from "react-icons/md";
+import { IoMdInformationCircle } from "react-icons/io";
+import { MdContactPhone } from "react-icons/md";
+import { MdWork } from "react-icons/md";
+import { IoIosHelpCircle } from "react-icons/io";
 export default function SideNav() {
   return (
     <div className="drawer lg:drawer-open">
@@ -12,12 +20,37 @@ export default function SideNav() {
           className="drawer-overlay"
         ></label>
         <ul className="menu bg-base-200 text-base-content min-h-full w-80 p-4 ">
-          {/* Sidebar content here */}
           <li>
-            <a>Sidebar Item 1</a>
+            <a className="flex items-center">
+              <span>
+                <BsArrowUpRightSquareFill size={25} />
+              </span>
+              Popular
+            </a>
           </li>
           <li>
-            <a>Sidebar Item 2</a>
+            <a className="flex items-center">
+              <span>
+                <IoGameControllerSharp size={25} />
+              </span>
+              Gaming
+            </a>
+          </li>
+          <li>
+            <a className="flex items-center">
+              <span>
+                <MdMovie size={25} />
+              </span>
+              Movies & TV
+            </a>
+          </li>
+          <li>
+            <a className="flex items-center">
+              <span>
+                <MdSportsBaseball size={25} />
+              </span>
+              Sports
+            </a>
           </li>
           <li>
             <label
@@ -41,6 +74,40 @@ export default function SideNav() {
               </svg>
             </label>
           </li>
+          {<div className="divider w-full"></div>}
+          <li>
+            <a className="flex items-center">
+              <span>
+                <IoMdInformationCircle size={25} />
+              </span>
+              About
+            </a>
+          </li>
+          <li>
+            <a className="flex items-center">
+              <span>
+                <MdContactPhone size={25} />
+              </span>
+              Contact
+            </a>
+          </li>
+          <li>
+            <a className="flex items-center">
+              <span>
+                <MdWork size={25} />
+              </span>
+              Careers
+            </a>
+          </li>
+          <li>
+            <a className="flex items-center">
+              <span>
+                <IoIosHelpCircle size={25} />
+              </span>
+              Support
+            </a>
+          </li>
+          {<div className="divider w-full"></div>}
         </ul>
       </div>
     </div>
