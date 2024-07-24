ce37b68d485cd8db7bce2c88da0314d9fa223b65
Cristian Se <114921009+NXTCHRIS@users.noreply.github.com>
2024-07-24 12:36:34 +0300

    fix close btn positioning

--- Social-Media-App-Clone/src/components/SideNav.jsx
+++ Social-Media-App-Clone/src/components/SideNav.jsx
@@ -52,28 +52,7 @@ export default function SideNav() {
               Sports
             </a>
           </li>
-          <li>
-            <label
-              htmlFor="my-drawer-2"
-              aria-label="close sidebar"
-              className=" drawer-button w-1/4  lg:hidden"
-            >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-6 w-6"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M6 18L18 6M6 6l12 12"
-                />
-              </svg>
-            </label>
-          </li>
+
           {<div className="divider w-full"></div>}
           <li>
             <a className="flex items-center">
@@ -108,6 +87,28 @@ export default function SideNav() {
             </a>
           </li>
           {<div className="divider w-full"></div>}
+          <li>
+            <label
+              htmlFor="my-drawer-2"
+              aria-label="close sidebar"
+              className=" drawer-button w-1/4  lg:hidden"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-6 w-6"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M6 18L18 6M6 6l12 12"
+                />
+              </svg>
+            </label>
+          </li>
         </ul>
       </div>
     </div>
