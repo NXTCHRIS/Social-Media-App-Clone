2400506bd7f5f3a24c6e0a56a8b2dc4a49703389
Cristian Se <114921009+NXTCHRIS@users.noreply.github.com>
2024-07-24 10:25:41 +0300

    remove

--- Social-Media-App-Clone/src/App.jsx
+++ Social-Media-App-Clone/src/App.jsx
@@ -1,14 +1,11 @@
 import NavBar from "./components/NavBar";
 import SideNav from "./components/SideNav";
-import { Box } from "@mui/material";
 function App() {
   return (
-    <div className="bg-slate-600">
-      <Box>
-        <NavBar />
-        <SideNav />
-      </Box>
-    </div>
+    <>
+      <NavBar />
+      <SideNav />
+    </>
   );
 }
 export default App;
