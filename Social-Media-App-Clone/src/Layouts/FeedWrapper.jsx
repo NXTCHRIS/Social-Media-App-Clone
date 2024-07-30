a5a6c1833523886760294e654e38d8108db63111
Cristian Se <114921009+NXTCHRIS@users.noreply.github.com>
2024-07-30 15:14:34 +0300

    fix padding issue

--- Social-Media-App-Clone/src/Layouts/FeedWrapper.jsx
+++ Social-Media-App-Clone/src/Layouts/FeedWrapper.jsx
@@ -3,7 +3,6 @@ import { v4 as uuidv4 } from "uuid";
 import Card from "../components/Card";
 export default function FeedWrapper() {
   const posts = useLoaderData();
-  console.log(posts.data.children);
   return (
     <div className="flex flex-col mx-auto gap-10 mt-32 mb-32 flex-1">
       {posts.data.children.map((item) => {
