835fe2d9f04148b685addae34c66f35e67aac415
Cristian Se <114921009+NXTCHRIS@users.noreply.github.com>
2024-07-30 15:10:54 +0300

    redesign card

--- Social-Media-App-Clone/src/components/Card.jsx
+++ Social-Media-App-Clone/src/components/Card.jsx
@@ -1,17 +1,28 @@
 /* eslint-disable no-undef */
 /* eslint-disable react/no-unknown-property */
 /* eslint-disable react/prop-types */
+import Button from "../components/Button";
+import { MdArrowCircleUp } from "react-icons/md";
+import { MdComment } from "react-icons/md";
+import { IoMdShareAlt } from "react-icons/io";
 import ReactPlayer from "react-player";
 import { v4 as uuidv4 } from "uuid";
 export default function Card({ post }) {
   return (
-    <div className="card card-compact bg-base-100 w-contain shadow-xl">
+    <div className="card card-compact bg-[rgba(13,17,20,255)] w-contain border-t rounded-none">
       <div className="card-body">
-        <h2 className="card-title text-sm lg:text-base">{post.data.title}</h2>
-        <div>
+        <h3 className="py-3">r/{post.data.subreddit}</h3>
+        <h1 className="card-title text-sm lg:text-xl py-3 text-white">
+          {post.data.title}
+        </h1>
+        <div className="">
           {post.data.media ? (
             <ReactPlayer
-              url={post.data.media.reddit_video.dash_url}
+              url={
+                post.data.media.reddit_video
+                  ? post.data.media.reddit_video.dash_url
+                  : post.data.media.reddit_video
+              }
               controls
               width="full"
               light={post.data.preview.images[0].source.url}
@@ -35,11 +46,27 @@ export default function Card({ post }) {
             </div>
           ) : null}
         </div>
-        <p className="text-xs lg:text-sm">{post.data.selftext}</p>
-        <div className="card-actions justify-end">
-          <button className="btn btn-primary text-[10px] lg:text-xs">
-            Buy Now
-          </button>
+        {post.data.selftext ? (
+          <p className="text-xs lg:text-sm h-28 overflow-auto py-3">
+            {post.data.selftext}
+          </p>
+        ) : null}
+        <div className="card-actions justify-start gap-2 w-6/12 py-3 ">
+          <Button
+            btnIcon={<MdArrowCircleUp size={25} color="white" />}
+            text={post.data.ups}
+            extCss={"text-wrap overflow-hidden text-white"}
+          />
+          <Button
+            btnIcon={<MdComment size={25} color="white" />}
+            text={post.data.num_comments}
+            extCss={"text-wrap overflow-hidden text-white"}
+          />
+          <Button
+            btnIcon={<IoMdShareAlt size={25} color="white" />}
+            text={"SHARE"}
+            extCss={"text-wrap overflow-hidden text-white"}
+          />
         </div>
       </div>
     </div>
